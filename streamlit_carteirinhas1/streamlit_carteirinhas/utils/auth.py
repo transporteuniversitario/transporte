42822a324844ce9c89b91214e07a5d87e22556af
transporteuniversitario <transporteescolarbelmonte2022@gmail.com>
2025-04-11 14:48:52 -0300

    Update auth.py

--- streamlit_carteirinhas1/streamlit_carteirinhas/utils/auth.py
+++ streamlit_carteirinhas1/streamlit_carteirinhas/utils/auth.py
@@ -1,5 +1,16 @@
 
+import json
+
 def autenticar_usuario(usuario, senha):
-    if usuario == "admin@admin.com" and senha == "admin123":
-        return True
-    return False
+    try:
+        with open("base_de_dados.json", "r", encoding="utf-8") as f:
+            dados = json.load(f)
+
+        for user in dados["usuarios"]:
+            if user["usuario"] == usuario and user["senha"] == senha:
+                return user["tipo"]  # "admin" ou "aluno"
+
+    except FileNotFoundError:
+        print("Arquivo base_de_dados.json não encontrado.")
+
+    return None
