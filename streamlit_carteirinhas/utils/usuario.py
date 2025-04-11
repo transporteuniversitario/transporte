6966dac0f3399109f850dfa7502235ac85bd43b2
transporteuniversitario <transporteescolarbelmonte2022@gmail.com>
2025-04-11 12:33:34 -0300

    Update usuario.py

--- streamlit_carteirinhas/utils/usuario.py
+++ streamlit_carteirinhas/utils/usuario.py
@@ -1,10 +1,10 @@
 import json
 
 def get_user_data():
-    with open("base_de_dados.json", "r", encoding="utf-8") as f:
-        return json.load(f)
-
-def salvar_dados(dados):
-    with open("base_de_dados.json", "w", encoding="utf-8") as f:
-        json.dump(dados, f, indent=4, ensure_ascii=False)
-
+    try:
+        with open("base_de_dados.json", "r", encoding="utf-8") as f:
+            data = json.load(f)
+        return data
+    except Exception as e:
+        print("Erro ao carregar base_de_dados.json:", e)
+        return None
